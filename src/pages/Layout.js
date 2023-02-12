49eb1b837221470d4fc55baf896bd14ebd730559
lukehongg <lukehongg@handong.ac.kr>
2023-02-12 16:04:13 +0900

    [#1]: 레이아웃 중간

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,24 +1,78 @@
-import { Link } from "react-router-dom";
+import { Typography } from '@mui/material';
+import { Box, Grid, Card } from '@mui/material';
+import { calcLength } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const Layout = () => {
-    return (
-        <div>
-            <h1>홈</h1>
-            <p>가장 먼저 보여지는 페이지입니다.</p>
-            <ul>
-                <li>
-                    <Link to="/youtube">Youtube</Link>
-                </li>
-                <li>
-                    <Link to="/instagram">Instagram</Link>
-                </li>
-                <li>
-                    <Link to="/model">Model</Link>
-                </li>
-            </ul>
-        </div>
-
-    );
+  return (
+    <div style={{ backgroundColor: '#1E1E1E', width: '100%', height: 'calc(100vh)' }}>
+      <Header />
+      <Box
+        container
+        display="flex"
+        justifyContent={'center'}
+        sx={{ height: '10%', pt: 20, pb: 20 }}
+      >
+        <Typography
+          variant="h1"
+          component="div"
+          sx={{ flexGrow: 1, color: 'white', textAlign: 'center' }}
+        >
+          Ink
+        </Typography>
+      </Box>
+      <Box container sx={{ height: '50%' }}>
+        <Grid
+          container
+          display="flex"
+          justifyContent={'center'}
+          lineHeight={'calc(45vh)'}
+          spacing={20}
+        >
+          <Grid item xs={2}>
+            <Box container display="flex" justifyContent={'center'}>
+              <Link to="/youtube">
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
+                >
+                  Youtube
+                </Typography>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={2}>
+            <Box container display="flex" justifyContent={'center'}>
+              <Link to="/instagram">
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
+                >
+                  Instagram
+                </Typography>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={2}>
+            <Box container display="flex" justifyContent={'center'}>
+              <Link to="/model">
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
+                >
+                  Model
+                </Typography>
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 };
 
 export default Layout;
