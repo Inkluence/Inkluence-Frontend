8077c93b27486d5627b90a90793dac826bcf9db2
suzie <clados422@gmail.com>
2023-02-14 14:16:55 +0900

    feat: Add Kakako Map

---
+++ src/components/youtube/KakaoMap.js
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from "react";
+
+function KakaoMap() {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        const container = mapRef.current;
+        const options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+        };
+        const map = new window.kakao.maps.Map(container, options);
+
+        // 마커가 표시될 위치입니다
+        var markerPosition  = new window.kakao.maps.LatLng(33.450701, 126.570667);
+
+        // 마커를 생성합니다
+        var marker = new window.kakao.maps.Marker({
+            position: markerPosition
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+
+        return () => {
+            map.remove();
+        };
+    },[]);
+
+    return <div ref={mapRef} style={{ width: "40%", height: "500px" }} />;
+}
+
+export default KakaoMap;
--- src/pages/common/ContactUs.js
+++ src/pages/common/ContactUs.js
@@ -1,13 +1,14 @@
 
 import NormalProcess from "../../assets/img/normal_process.png";
 import InkProcess from "../../assets/img/ink_process.png";
+import KakaoMap from "../../components/youtube/KakaoMap";
 const ContactUs = () => {
     return(
         <div id="contact" className="youtube-process">
             <div className="text-white">
                 <span className="title">Contact Us</span>
                 <div className="content">
-
+                    <KakaoMap/>
                 </div>
             </div>
         </div>
